a7d6b18c2cb23a8f3b40f2a6dc02e04be3bd6664
acrylic-style <me@acrylicstyle.xyz>
2024-08-04 13:18:07 +0900

    add reinforced deepslate

--- src/main/java/net/azisaba/afnw/afnwcore2/commands/AfnwCommand.java
+++ src/main/java/net/azisaba/afnw/afnwcore2/commands/AfnwCommand.java
@@ -80,6 +80,7 @@ public record AfnwCommand(JavaPlugin plugin, PlayerData playerData) implements C
           itemList.add(Material.DIAMOND);
           itemList.add(Material.GOLD_INGOT);
           itemList.add(Material.NETHERITE_UPGRADE_SMITHING_TEMPLATE);
+          itemList.add(Material.REINFORCED_DEEPSLATE);
         }
       }
       return new ItemStack(itemList.get(random.nextInt(itemList.size() - 1)), amount);
